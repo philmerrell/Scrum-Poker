97fd393c4a1c8bb86fdc9dcb7f23e170b8361cc3
Phil Merrell <philmerrell@boisestate.edu>
2016-11-19 14:35:59 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { PokerComponent } from './poker/poker.component';
+import { CardComponent } from './card/card.component';
+import { PokerService } from './poker.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokerComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: PokerComponent
+      },
+      {
+        path: 'card',
+        component: CardComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [
+    PokerService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
